1b376413cf39d5c0ff0ed94188dd725266dd96b6
Aswin Andro <aswin.knight@gmail.com>
2025-10-08 11:14:35 +0400

    error
    error

--- patient-service/src/main/java/com/andro/pm/patient_service/exception/GlobalExceptionHandler.java
+++ patient-service/src/main/java/com/andro/pm/patient_service/exception/GlobalExceptionHandler.java
@@ -1,5 +1,7 @@
 package com.andro.pm.patient_service.exception;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -10,10 +12,20 @@ import java.util.Map;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
-        @ExceptionHandler(MethodArgumentNotValidException.class)
+    private static final Logger log = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
         public ResponseEntity<Map<String, String>> handleValidationException(MethodArgumentNotValidException ex){
             Map<String, String> errors = new HashMap<>();
             ex.getBindingResult().getFieldErrors().forEach(error -> errors.put(error.getField(), error.getDefaultMessage()));
             return ResponseEntity.badRequest().body(errors);
+        }
+
+        @ExceptionHandler(EmailAlreadyExistsException.class)
+        public ResponseEntity<Map<String, String>> handleEmailAlreadyExistsException(EmailAlreadyExistsException ex){
+            log.warn("Email Address already exists{}", ex.getMessage());
+            Map<String, String> error = new HashMap<>();
+            error.put("message", ex.getMessage());
+            return ResponseEntity.badRequest().body(error);
         }
 }
