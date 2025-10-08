e41c25fd161f50850eb845cad98bfa16e4a56a05
Aswin Andro <aswin.knight@gmail.com>
2025-10-08 10:12:54 +0400

    Initial commit

---
+++ patient-service/src/main/java/com/andro/pm/patient_service/service/PatientService.java
@@ -0,0 +1,30 @@
+package com.andro.pm.patient_service.service;
+
+import com.andro.pm.patient_service.dto.PatientRequestDTO;
+import com.andro.pm.patient_service.dto.PatientResponseDTO;
+import com.andro.pm.patient_service.mapper.PatientMapper;
+import com.andro.pm.patient_service.model.Patient;
+import com.andro.pm.patient_service.repository.PatientRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class PatientService {
+    private final PatientRepository patientRepository;
+
+    public PatientService(PatientRepository patientRepository){
+        this.patientRepository = patientRepository;
+    }
+
+    public List<PatientResponseDTO> getPatients() {
+        List<Patient> patients = patientRepository.findAll();
+        return patients.stream().map(PatientMapper::toDTO).toList();
+    }
+
+    public  PatientResponseDTO createPatient(PatientRequestDTO patientRequestDTO){
+        Patient newPatient = patientRepository.save(PatientMapper.toModel(patientRequestDTO));
+        return PatientMapper.toDTO(newPatient);
+    }
+
+}
